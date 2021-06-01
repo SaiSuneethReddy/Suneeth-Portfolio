3d874695ff41a171592dace3825fcd65f7361fd0
Sai <sai.reddy@tadigital.com>
2021-06-01 14:13:11 +0530

    build changes

---
+++ build/precache-manifest.dbbcd701f55263e7746fcc08227bbcd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb1902668aa82686ab687940722878f",
+    "url": "/SaiSuneethReddy/Suneeth-Portfolio/index.html"
+  },
+  {
+    "revision": "7a4e07e4d210695701b9",
+    "url": "/SaiSuneethReddy/Suneeth-Portfolio/static/css/2.49ecd109.chunk.css"
+  },
+  {
+    "revision": "78e59d6b106b574395ff",
+    "url": "/SaiSuneethReddy/Suneeth-Portfolio/static/css/main.d269bd61.chunk.css"
+  },
+  {
+    "revision": "7a4e07e4d210695701b9",
+    "url": "/SaiSuneethReddy/Suneeth-Portfolio/static/js/2.6a80857f.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/SaiSuneethReddy/Suneeth-Portfolio/static/js/2.6a80857f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e59d6b106b574395ff",
+    "url": "/SaiSuneethReddy/Suneeth-Portfolio/static/js/main.b237bdb4.chunk.js"
+  },
+  {
+    "revision": "3f7d0eb6e66d6ff26758",
+    "url": "/SaiSuneethReddy/Suneeth-Portfolio/static/js/runtime-main.efcf8654.js"
+  }
+]);
